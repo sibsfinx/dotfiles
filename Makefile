ffe5504e5f96c2cafafc96b6e50a226f63d01568
Alexander Mescheryakov <sibsfinx@gmail.com>
2024-03-12 14:23:22 +0100

    upd makefile

--- Makefile
+++ Makefile
@@ -1,3 +1,15 @@
+.PHONY: run-all dev brew
+
+.DEFAULT_GOAL := run-all
+
+run-all:
+	make brew
+	make dev
+	source ~/.zshrc
+	source ~/.zprofile
+
+dev:
+	./dependencies/dev.sh
 
 brew:
-	./homebrew.sh
+	./dependencies/homebrew.sh
---
+++ install/homebrew.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+brew update
+brew install git git-lfs
+brew install wget curl
+brew install zsh the_silver_searcher fzf tmux direnv
+brew install rbenv && rbenv init
+brew install docker ansible
+brew install postgres mysql sqlite
+
+brew tap codeclimate/formulae
+brew install codeclimate
+
+brew install --cask visual-studio-code around google-chat figma tunnelblick krisp
+brew install --cask google-chrome firefox telegram spotify
+brew install --cask 1password 1password-cli lastpass lastpass-cli outline-manager mullvadvpn
+
+brew install licecap ffmpeg gifsicle
+
+brew install browserstacklocal ngrok
+
+brew install imageoptim unrar
+#	graphviz 
+# 	aria2
+
+# brew install mycli golang vagrant composer
+# brew install overmind hivemind
+
+# brew install --cask maccy
+
+# brew install openjdk \
+# 	gradle
+# 	kotlin
+

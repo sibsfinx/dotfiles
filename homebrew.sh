3ef2da7202c4fe05b7e76c85f2a8054d873d3534
Alexander Mescheryakov <sibsfinx@gmail.com>
2024-03-12 14:21:25 +0100

    upd brew and dev scripts

--- homebrew.sh
+++ homebrew.sh
@@ -1,47 +1,45 @@
 #!/bin/bash
 brew update
-brew install git
-brew install git-lfs
-brew install wget
-brew install zsh
+brew install git git-lfs
+brew install wget curl
+brew install zsh the_silver_searcher
 brew install direnv
 brew install rbenv && rbenv init
 brew install ansible
 brew install docker
-brew install openjdk
-brew install mysql
+brew install openjdk mysql postgres
 # brew install aria2
 # brew install gradle
 # brew install kotlin
 brew install ngrok
-brew install nginx
-brew install graphviz
+# brew install nginx
+brew install imageoptim graphviz
 brew install unrar
-brew install imageoptim
+
 # brew install mycli
 # brew install golang
 # brew install vagrant
 # brew install composer
 # brew install --cask maccy
+
 brew install fzf
 # brew install ripgrep
-brew install postgres
-# brew install postgres@12
-# brew uninstall postgres && brew install postgres
-# brew uninstall --ignore-dependencies postgres && brew install postgres
+
 brew tap codeclimate/formulae
 brew install codeclimate
+
 # brew install hivemind
-# brew install tmux && brew install overmind
+brew install tmux overmind
 # brew install rm-improved
-#  6606  brew install twitter
+# brew install rabbitmq
+
 brew install licecap
 brew install ffmpeg
 brew install gifsicle
-# brew install rabbitmq
-brew install --cask visual-studio-code
-brew install --cask kite
+
+brew install --cask visual-studio-code around google-chat figma tunnelblick
+# brew install --cask kite
+
+brew install --cask google-chrome firefox telegram spotify
+
 brew install browserstacklocal
-brew install --cask telegram
-brew install --cask google-chat
-brew install --cask spotify
